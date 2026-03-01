4282adaf1cedd184944f9a713e9fc524c544f2c8
Mourad Essebane <essebane.mourad@gmail.com>
2026-03-01 19:18:12 +0100

    Added unit tests for GlidedTros

--- CSharp/GildedTros.App/GildedTrosTest.cs
+++ CSharp/GildedTros.App/GildedTrosTest.cs
@@ -3,15 +3,173 @@ using Xunit;
 
 namespace GildedTros.App
 {
-    public class GildedTrosTest
+    public class GildedRoseTests
     {
-        [Fact]
-        public void foo()
+        private static Item CreateItem(string name, int sellIn, int quality)
+            => new Item { Name = name, SellIn = sellIn, Quality = quality };
+
+        private void UpdateOneDay(Item item)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedTros app = new GildedTros(Items);
+            var app = new GildedTros(new List<Item> { item });
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+        }
+
+        // ================================
+        // NORMAL ITEMS
+        // ================================
+
+        [Fact]
+        public void NormalItem_DecreasesSellInBy1()
+        {
+            var item = CreateItem("Normal Item", 10, 20);
+            UpdateOneDay(item);
+            Assert.Equal(9, item.SellIn);
+        }
+
+        [Fact]
+        public void NormalItem_DecreasesQualityBy1()
+        {
+            var item = CreateItem("Normal Item", 10, 20);
+            UpdateOneDay(item);
+            Assert.Equal(19, item.Quality);
+        }
+
+        [Fact]
+        public void NormalItem_DegradesTwiceAsFastAfterExpiration()
+        {
+            var item = CreateItem("Normal Item", 0, 20);
+            UpdateOneDay(item);
+            Assert.Equal(18, item.Quality);
+        }
+
+        [Fact]
+        public void NormalItem_QualityNeverNegative()
+        {
+            var item = CreateItem("Normal Item", 5, 0);
+            UpdateOneDay(item);
+            Assert.Equal(0, item.Quality);
+        }
+
+        // ================================
+        // GOOD WINE
+        // ================================
+
+        [Fact]
+        public void GoodWine_IncreasesQuality()
+        {
+            var item = CreateItem("Good Wine", 10, 20);
+            UpdateOneDay(item);
+            Assert.Equal(21, item.Quality);
+        }
+
+        [Fact]
+        public void GoodWine_IncreasesTwiceAsFastAfterExpiration()
+        {
+            var item = CreateItem("Good Wine", 0, 20);
+            UpdateOneDay(item);
+            Assert.Equal(22, item.Quality);
+        }
+
+        [Fact]
+        public void GoodWine_QualityNeverMoreThan50()
+        {
+            var item = CreateItem("Good Wine", 5, 50);
+            UpdateOneDay(item);
+            Assert.Equal(50, item.Quality);
+        }
+
+        // ================================
+        // LEGENDARY ITEM
+        // ================================
+
+        [Fact]
+        public void LegendaryItem_DoesNotDecreaseSellIn()
+        {
+            var item = CreateItem("B-DAWG Keychain", 10, 80);
+            UpdateOneDay(item);
+            Assert.Equal(10, item.SellIn);
+        }
+
+        [Fact]
+        public void LegendaryItem_DoesNotChangeQuality()
+        {
+            var item = CreateItem("B-DAWG Keychain", 10, 80);
+            UpdateOneDay(item);
+            Assert.Equal(80, item.Quality);
+        }
+
+        // ================================
+        // BACKSTAGE PASSES
+        // ================================
+
+        [Theory]
+        [InlineData("Backstage passes for Re:factor")]
+        [InlineData("Backstage passes for HAXX")]
+        public void BackstagePass_IncreasesBy1_WhenMoreThan10Days(string name)
+        {
+            var item = CreateItem(name, 15, 20);
+            UpdateOneDay(item);
+            Assert.Equal(21, item.Quality);
+        }
+
+        [Theory]
+        [InlineData("Backstage passes for Re:factor")]
+        [InlineData("Backstage passes for HAXX")]
+        public void BackstagePass_IncreasesBy2_When10DaysOrLess(string name)
+        {
+            var item = CreateItem(name, 10, 20);
+            UpdateOneDay(item);
+            Assert.Equal(22, item.Quality);
+        }
+
+        [Theory]
+        [InlineData("Backstage passes for Re:factor")]
+        [InlineData("Backstage passes for HAXX")]
+        public void BackstagePass_IncreasesBy3_When5DaysOrLess(string name)
+        {
+            var item = CreateItem(name, 5, 20);
+            UpdateOneDay(item);
+            Assert.Equal(23, item.Quality);
+        }
+
+        [Theory]
+        [InlineData("Backstage passes for Re:factor")]
+        [InlineData("Backstage passes for HAXX")]
+        public void BackstagePass_DropsToZeroAfterConcert(string name)
+        {
+            var item = CreateItem(name, 0, 20);
+            UpdateOneDay(item);
+            Assert.Equal(0, item.Quality);
+        }
+
+        [Theory]
+        [InlineData("Backstage passes for Re:factor")]
+        [InlineData("Backstage passes for HAXX")]
+        public void BackstagePass_QualityNeverMoreThan50(string name)
+        {
+            var item = CreateItem(name, 5, 50);
+            UpdateOneDay(item);
+            Assert.Equal(50, item.Quality);
+        }
+
+        // ================================
+        // GLOBAL RULES
+        // ================================
+
+        [Fact]
+        public void Quality_NeverNegative()
+        {
+            var item = CreateItem("Normal Item", -1, 0);
+            UpdateOneDay(item);
+            Assert.Equal(0, item.Quality);
+        }
+
+        [Fact]
+        public void Quality_NeverExceeds50()
+        {
+            var item = CreateItem("Good Wine", -1, 50);
+            UpdateOneDay(item);
+            Assert.Equal(50, item.Quality);
         }
     }
 }
